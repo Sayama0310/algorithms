cc955c147ab9e3a490dedf4454cad05254421206
Sayama0310 <montblanc3060@gmail.com>
2023-07-22 06:22:11 +0900

    [test] Added tests for the Sieve of Eratosthenes.

--- src/arithmetic/mod.rs
+++ src/arithmetic/mod.rs
@@ -19,3 +19,14 @@ pub fn sieve_of_eratosthenes(n: usize) -> Vec<usize> {
         .filter_map(|(i, &is_prime)| if is_prime { Some(i) } else { None })
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sieve_of_eratosthenes() {
+        assert_eq!(sieve_of_eratosthenes(10), vec![2, 3, 5, 7]);
+        assert_eq!(sieve_of_eratosthenes(20), vec![2, 3, 5, 7, 11, 13, 17, 19]);
+    }
+}
